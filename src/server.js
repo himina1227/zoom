121227ddd6e5727870e0a285baf9db5836202062
nakgyeom <himina1227@gmail.com>
2023-02-15 15:09:41 +0900

    websocket 설정

--- src/server.js
+++ src/server.js
@@ -1,7 +1,18 @@
+import http from "http";
 import express from "express";
+import path from "path";
+import WebSocket, {WebSocketServer} from "ws";
 
+const __dirname = path.resolve();
 const app = express();
 
-console.log("hello");
-
-app.listen(3000);
+app.set("view engine", "pug");
+app.set("views", __dirname + "/src/views");
+app.use("/public", express.static(__dirname + "/src/public"));
+console.log(__dirname);
+app.get("/", (req, res) => res.render("home"));
+const handleListen = () => console.log("Listening on http://localhost:3000");
+// app.listen(3500, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocketServer({server});
+app.listen(3000, handleListen);
