9f3ef732295fc44986012ca523e6cd502fa420c5
nakgyeom <himina1227@gmail.com>
2023-02-16 17:06:36 +0900

    - 2.1 installing SocketIO

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import http from "http";
 import express from "express";
 import path from "path";
-import webSocket, {WebSocketServer} from "ws";
+import {Server} from "socket.io";
+// import webSocket, {WebSocketServer} from "ws";
 
 const __dirname = path.resolve();
 const app = express();
@@ -14,35 +15,40 @@ app.get("/", (req, res) => res.render("home"));
 const handleListen = () => console.log("Listening on http://localhost:3000");
 // app.listen(3000, handleListen);
 
-const server = http.createServer(app);
-const wss = new WebSocketServer({server});
+const httpServer = http.createServer(app);
+const wsServer = new Server(httpServer);
 
-const sockets = [];
+wsServer.on("connection", (socket) => {
+    console.log(socket);
+})
 
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "Anon";
-    console.log("Connected to Browser!");
-    socket.on("close", () => console.log("Disconnected from the Browser"));
-    // 특정 소켓에서 메시지 받기
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-        switch (message.type) {
-            case "new_message":
-                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
-            case "nickname":
-                socket["nickname"] = message.payload;
-                // console.log(parsed.payload);
-        }
-
-        // if (parsed.type === "new_message") {
-        //     sockets.forEach(aSocket => aSocket.send(parsed.payload));
-        // } else if (parsed.type === "nickname") {
-        //     console.log(parsed.payload);
-        // }
-    });
-    // 메시지를 브라우저로 전달
-    socket.send("hello");
-});
-
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
+// const wss = new WebSocketServer({server});
+//
+// const sockets = [];
+//
+// wss.on("connection", (socket) => {
+//     sockets.push(socket);
+//     socket["nickname"] = "Anon";
+//     console.log("Connected to Browser!");
+//     socket.on("close", () => console.log("Disconnected from the Browser"));
+//     // 특정 소켓에서 메시지 받기
+//     socket.on("message", (msg) => {
+//         const message = JSON.parse(msg);
+//         switch (message.type) {
+//             case "new_message":
+//                 sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
+//             case "nickname":
+//                 socket["nickname"] = message.payload;
+//                 // console.log(parsed.payload);
+//         }
+//
+//         // if (parsed.type === "new_message") {
+//         //     sockets.forEach(aSocket => aSocket.send(parsed.payload));
+//         // } else if (parsed.type === "nickname") {
+//         //     console.log(parsed.payload);
+//         // }
+//     });
+//     // 메시지를 브라우저로 전달
+//     socket.send("hello");
+// });
