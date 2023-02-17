1f96f502dcdee599e51fdecfd99a17e3a5b33f67
nakgyeom <himina1227@gmail.com>
2023-02-17 13:41:20 +0900

    - 2.2 SocketIO is Amazing

--- src/server.js
+++ src/server.js
@@ -20,6 +20,13 @@ const wsServer = new Server(httpServer);
 
 wsServer.on("connection", (socket) => {
     console.log(socket);
+    // socket.on("enter_room", (msg) => console.log(msg));
+    socket.on("enter_room", (msg, done) => {
+        console.log(msg);
+        setTimeout(() => {
+            done();
+        }, 10000)
+    })
 })
 
 httpServer.listen(3000, handleListen);
